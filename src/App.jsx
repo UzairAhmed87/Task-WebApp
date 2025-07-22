e092d5f381b949ce9b35c2d72961cbcd4e841482
UzairAhmed87 <uzairahmed1574@gmail.com>
2025-07-23 02:17:32 +0500

    added vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import ChartPage from './components/ChartPage';
 import TablePage from './components/TablePage';
+import { Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Routes>
+         <Route path="/" element={<Navigate to="/chart" replace />} />
         <Route path="/chart" element={<ChartPage />} />
         <Route path="/table" element={<TablePage />} />
-        <Route path="*" element={<ChartPage />} /> {/* Default route */}
+        <Route path="*" element={<ChartPage />} /> Default route
       </Routes>
     </div>
   );
